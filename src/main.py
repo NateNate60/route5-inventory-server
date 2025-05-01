3484fadc62bb484dc41be5a39aa61af493167075
Nathan Lim <nathanlim2004@gmail.com>
2025-05-01 10:27:00 -0700

    Implement /inventory/add and /inventory/consign

--- src/main.py
+++ src/main.py
@@ -1,4 +1,92 @@
 import flask
-import mysql
+import pymongo
+from datetime import datetime
+
+import config
+from authentication import authenticate
+
+URI = f"mongodb+srv://{config.MONGODB_USERNAME}:{config.MONGODB_PASSWORD}@{config.MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName={config.MONGODB_APPNAME}"
+DATABASE = pymongo.MongoClient(URI, server_api=pymongo.server_api.ServerApi('1'))['route5']
+
 
 app = flask.Flask(__name__)
+
+@app.route("/v1/inventory/add", methods=["POST"])
+def add_item ():
+    data = flask.request.get_json()
+    items = []
+    price_total = 0
+    for item in data["items"]:
+        item["consignor_name"] = ""
+        item["consignor_contact"] = ""
+        if item["type"] not in ("card", "slab", "sealed"):
+            return flask.Response(
+                {
+                    "error": "Invalid item type"
+                }, status=400
+            )
+        if item["type"] == "sealed":
+            # Increment the number of this sealed product in inventory
+            result = DATABASE["inventory"].update_one({"id": item["id"]}, {"$inc": {"quantity": item["quantity"]}})
+            # If nothing was modified, it's not in the database!
+            if result.modified_count == 0:
+                DATABASE["inventory"].insert_one(item)
+        else:
+            result = DATABASE["inventory"].replace_one({"id": item["id"]}, 
+                                                       replacement=item,
+                                                       upsert=True)
+            item["quantity"] = 1
+        items.append({
+            "id": item["id"],
+            "acquired_price": item["acquired_price"],
+            "quantity": item["quantity"]
+        })
+        price_total += item["acquired_price"]
+    txid = f"TXB{DATABASE['buys'].count_documents({})}"
+    DATABASE["buys"].insert_one(
+        {
+            "acquired_date": datetime.today().strftime('%Y-%m-%d'),
+            "acquired_from_name": data["acquired_from_name"],
+            "acquired_from_contact": data["acquired_from_contact"],
+            "acquired_price_total": price_total,
+            "items": items,
+            "txid": txid
+        }
+    )
+
+    return {"txid": txid}
+
+@app.route("/v1/inventory/consign", methods=["POST"])
+def consign_item ():
+    item = flask.request.get_json()
+    if item["type"] not in ("card", "slab", "sealed"):
+        return flask.Response(
+            {
+                "error": "Invalid item type"
+            }, status=400
+        )
+    if len(item["id"]) == 12:
+        # This is a UPC and should be rejected
+        return flask.Response(
+            {
+                "error": "No UPCs allowed"
+            }, status=400
+        ) 
+    result = DATABASE["inventory"].replace_one(filter={"id": item["id"]}, 
+                                                replacement=item,
+                                                upsert=True)
+
+    txid = f"TXC{DATABASE['consignments'].count_documents({})}"
+    DATABASE["consignments"].insert_one(
+        {
+            "consign_date": datetime.today().strftime('%Y-%m-%d'),
+            "consignor_name": item["consignor_name"],
+            "consignor_contact": item["consignor_contact"],
+            "sale_price": item["sale_price"],
+            "item": item["id"],
+            "txid": txid
+        }
+    )
+
+    return {"txid": txid}
+
