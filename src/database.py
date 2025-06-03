cd8a5256d7c9d8d66af8c2423d6b184f7e1bc63d
Nathan Lim <nathanlim2004@gmail.com>
2025-06-03 15:20:45 -0700

    Add test mode

--- src/database.py
+++ src/database.py
@@ -2,4 +2,4 @@ import config
 import pymongo
 
 URI = f"mongodb+srv://{config.MONGODB_USERNAME}:{config.MONGODB_PASSWORD}@{config.MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName={config.MONGODB_APPNAME}"
-DATABASE = pymongo.MongoClient(URI, server_api=pymongo.server_api.ServerApi('1'))['route5']
+DATABASE = pymongo.MongoClient(URI, server_api=pymongo.server_api.ServerApi('1'))['route5' if not config.TEST else 'route5test']
