b3236fb2041c3bb814eeb5cbba2a6f6a6a0baff9
Nathan Lim <nathanlim2004@gmail.com>
2025-06-27 09:48:09 -0700

    Change timeout to 10 days

--- src/database.py
+++ src/database.py
@@ -4,4 +4,4 @@ from mysql import connector
 
 URI = f"mongodb+srv://{config.MONGODB_USERNAME}:{config.MONGODB_PASSWORD}@{config.MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName={config.MONGODB_APPNAME}"
 DATABASE = pymongo.MongoClient(URI, server_api=pymongo.server_api.ServerApi('1'))['route5' if not config.TEST else 'route5test']
-MYSQL = connector.connect(host="localhost", user=config.MYSQL_USER, password=config.MYSQL_PASSWORD, database="route5prices")
+MYSQL = connector.connect(host="localhost", user=config.MYSQL_USER, password=config.MYSQL_PASSWORD, database="route5prices", connection_timeout=864000)
