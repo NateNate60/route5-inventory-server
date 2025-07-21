37009fdc32d1aa1236c0aa393882b1a1d01b327b
Nathan Lim <nathanlim2004@gmail.com>
2025-07-21 12:47:08 -0700

    Implement PATCH /v1/settings/rates

--- src/handlers/settings.py
+++ src/handlers/settings.py
@@ -9,7 +9,29 @@ settings = flask.Blueprint('settings', __name__)
 @jwt_required()
 def buyrates () :
     if flask.request.method == "PATCH":
-        return flask.Response('{"error": "unimplemented"}', status=501)
+        data = flask.request.json
+
+        # validation
+        MALFORMED_DATA = flask.Response('{"error": "Missing one or more required fiels"}', status=422)
+        if "cutoffs" not in data or "cash_rates" not in data or "credit_rates" not in data:
+            return MALFORMED_DATA
+        for i in ("card", "slab", "sealed"):
+            if i not in data["cutoffs"] or i not in data["cash_rates"] or i not in data["credit_rates"]:
+                return MALFORMED_DATA
+            if type(data["cutoffs"][i]) != list or type(data["cash_rates"][i]) != list or type(data["credit_rates"][i]) != list:
+                return MALFORMED_DATA
+            
+            cutoffs_length = len(data["cutoffs"][i])
+
+            if len(data["cash_rates"][i]) != cutoffs_length + 1 or len(data["cash_rates"][i]) != cutoffs_length + 1:
+                return MALFORMED_DATA
+        
+        data["id"] = "rates"
+
+        DATABASE["settings"].replace_one({"id": "rates"}, data)
+
+        return flask.Response(status=204)
+
     else:
         db_value = DATABASE["settings"].find_one({"id": "rates"})
         del db_value["_id"]
